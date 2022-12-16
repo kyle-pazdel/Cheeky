609f58202f0698f4e978ea641799bae34368857d
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-12-16 12:25:27 -0800

    Adds create action to notes.js in actionTypes

--- app/actions/noteActions.js
+++ app/actions/noteActions.js
@@ -1,17 +1,40 @@
-import { types } from "@babel/core";
+import {types} from '@babel/core';
 
-export function fetchNotes() = {
+export function fetchNotes() {
   return async dispatch => {
     dispatch({type: types.FETCH_NOTES});
     try {
-        let response = await fetch('http://localhost:3000/performers.json');
+      let response = await fetch('http://localhost:3000/performers.json');
       if (response.status !== 200) {
-        throw new Error('FETCH_ERROR')
+        throw new Error('FETCH_ERROR');
       }
-      response = await response.json()
+      response = await response.json();
       dispatch({type: types.FETCH_NOTES_SUCCESS, data: response});
     } catch (error) {
-      dispatch(type: types.FETCH_NOTES_FAILURE, error);
+      dispatch({type: types.FETCH_NOTES_FAILURE, error});
+    }
+  };
+}
+
+export function createNote(note) {
+  return async dispatch => {
+    dispatch({type: types.CREATE_NOTE});
+    try {
+      let response = await fetch('http://localhost:3000/performers.json', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({note}),
+      });
+      if (response.statys !== 200) {
+        throw new Error('FETCH_ERROR');
+      }
+      response = await response.json();
+      dispatch({type: types.CREATE_NOTE_SUCCESS, data: response});
+    } catch (error) {
+      dispatch({type: types.CREATE_NOTE_FAILURE, error});
     }
   };
 }

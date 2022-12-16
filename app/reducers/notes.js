b60f62d35b4885e95cac1dbcff23fe947f3c120a
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-12-16 12:46:51 -0800

    Adds switch cases for fetch actions in notes.js in reducers

--- app/reducers/notes.js
+++ app/reducers/notes.js
@@ -8,6 +8,25 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case 'FETCH_NOTES':
+      return {
+        ...state,
+        status: 'loading',
+        error: null,
+      };
+    case 'FETCH_NOTES_SUCCESS':
+      return {
+        ...state,
+        status: 'success',
+        data: action.data,
+        error: null,
+      };
+    case 'FETCH_NOTES_FAILURE':
+      return {
+        ...state,
+        status: 'failure',
+        error: action.error,
+      };
     default:
       return state;
   }

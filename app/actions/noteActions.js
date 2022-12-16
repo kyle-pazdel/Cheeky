82748bdcef4118acb7a69af12cbdb3de62e87730
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-12-16 12:13:14 -0800

    Adds fetch action to notesAction.js

---
+++ app/actions/noteActions.js
@@ -0,0 +1,17 @@
+import { types } from "@babel/core";
+
+export function fetchNotes() = {
+  return async dispatch => {
+    dispatch({type: types.FETCH_NOTES});
+    try {
+        let response = await fetch('http://localhost:3000/performers.json');
+      if (response.status !== 200) {
+        throw new Error('FETCH_ERROR')
+      }
+      response = await response.json()
+      dispatch({type: types.FETCH_NOTES_SUCCESS, data: response});
+    } catch (error) {
+      dispatch(type: types.FETCH_NOTES_FAILURE, error);
+    }
+  };
+}

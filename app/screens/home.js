18b54c31be85e40fbf95c093d2a378755e6ef388
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-12-20 10:02:28 -0800

    Corrects function syntax in home.js

--- app/screens/home.js
+++ app/screens/home.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
-import { ScrollView } from 'react-native';
+import {View, ScrollView} from 'react-native';
 
 export default class Home extends Component {
-  render(
-    return(
-      <ScrollView></ScrollView>
-    )
-  )
+  componentDidMount() {
+    this.props.actions.fetchNotes();
+  }
+
+  _renderNotes() {
+    const {data} = this.props.notes;
+    return <View>{data.map(note => this._renderNote(note))}</View>;
+  }
+
+  render() {
+    return <ScrollView></ScrollView>;
+  }
 }
